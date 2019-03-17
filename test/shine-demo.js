9d6c61ec93d52e812c6adeffbb82f4a476216ad8
shine130 <shine130@vip.qq.com>
2019-03-17 17:37:55 +0800

    init

---
+++ test/shine-demo.js
@@ -0,0 +1,13 @@
+const chai = require('chai')
+const should = chai.should()
+
+describe('ShineDemo',function(){
+    it('使用 should 风格的断言测试',function(){
+        var value = 'hello'
+        value.should.exist.and.be.a('string').and.equal('hello').and.have.length(5)
+        // value.should.be.a('string')
+        // value.should.equal('hello')
+        // value.should.not.equal('您好')
+        // value.should.have.length(5)
+    })
+})
