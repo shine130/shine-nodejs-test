82514036705e5c3fab0f190e3ee7cecd75aebc33
shine <shine130@vip.qq.com>
2019-03-18 14:13:46 +0800

    'Node.js测试'

--- test/shine-demo.js
+++ test/shine-demo.js
@@ -1,13 +1,34 @@
 const chai = require('chai')
-const should = chai.should()
+const expect = chai.expect
+const ShineDemo = require('../lib/shine-demo')
+
+var demo = new ShineDemo()
 
 describe('ShineDemo',function(){
-    it('使用 should 风格的断言测试',function(){
-        var value = 'hello'
-        value.should.exist.and.be.a('string').and.equal('hello').and.have.length(5)
-        // value.should.be.a('string')
-        // value.should.equal('hello')
-        // value.should.not.equal('您好')
-        // value.should.have.length(5)
+    it('给汽车引擎加水是不能接受的事情',function(){
+        expect(function(){
+            demo.engine('water')
+        }).to.throw('not accept')
     })
+
+    // it('加载豆瓣API返回的内容里应该包含subjects属性',function(done){
+    //     demo.fetchData('top250',function(data){
+    //         expect(data).to.have.property('subjects')
+    //         done()
+    //     })
+    // })
+
+    // this.timeout(5000)
+
+    // it('一段时间以后返回数据',function(done){
+    //     demo.waitTwoSecond('hello',function(data){
+    //         expect(data).to.equal('您好')
+    //         done()
+    //     })
+    // })
+
+    // it('单价是10块钱的3件商品小计金额应该是30块',function(){
+    //     var subtotal = demo.subtotal(10,3)
+    //     expect(subtotal).to.equal(30)
+    // })
 })
